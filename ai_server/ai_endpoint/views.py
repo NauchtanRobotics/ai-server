4c74bdd0c1bada0395fdcdc4b56e3600b6a8cba9
David <davidmacnaugh@gmail.com>
2021-11-12 09:55:37 +1000

    AE-1 Draft subprocess run command.

--- ai_server/ai_endpoint/views.py
+++ ai_server/ai_endpoint/views.py
@@ -29,7 +29,28 @@ def ajax_view(request):
             logging.log(level=logging.CRITICAL, msg=f"Project_ref: {project_ref}")
         else:
             logging.log(level=logging.CRITICAL, msg="No 'project_ref' key found in post.")
-            return JsonResponse({"msg": "No 'project_identifier' key found in post."})
+            return JsonResponse({"error": "No 'project_identifier' key found in post."})
+
+        if "classifier" in request.POST.keys():
+            if request.POST["classifier"] == "sealed_road_defects":
+                model_path = Path("/home/david/addn_repos/yolov5/runs/train")
+            else:
+                msg = "Nominated classifier is unknown"
+                logging.log(level=logging.CRITICAL, msg=msg)
+                return JsonResponse({"error": msg})
+        else:
+            msg = "No 'classifier' key found in post."
+            logging.log(level=logging.CRITICAL, msg=msg)
+            return JsonResponse({"error": msg})
+
+        if "model_version" in request.POST.keys():
+            model_path = model_path / "Collation_6_unweighted_equal_fitness_scale50pcnt"
+        else:
+            msg = "No 'model_version' key found in post."
+            logging.log(level=logging.CRITICAL, msg=msg)
+            return JsonResponse({"error": msg})
+
+        model_path = model_path / "weights" / "best.pt"
 
         if hasattr(request, "FILES"):
             in_memory_uploaded_file = dict(request.FILES)["image"][0]  # request.FILES.get('image')
@@ -39,14 +60,52 @@ def ajax_view(request):
             with open(str(dst_file_path), 'wb+') as destination:
                 for chunk in in_memory_uploaded_file.chunks():
                     destination.write(chunk)
-            # Subprocess call here to detect.py in yoloV5.
-            # if annotations.txt:
-            #    detections = read lines annotations.txt file into list.
-            # else:
-            detections = []
+
+            run_name = "tmp"
+            tmp_folder = Path("/home/david/addn_repos/yolov5/runs/detect") / run_name
+            tmp_folder.unlink(missing_ok=True)
+            tmp_folder.mkdir(parents=True)
+
+            cmd = [
+                "/home/david/addn_repos/yolov5/venv/bin/python",
+                "/home/david/addn_repos/yolov5/detect.py",
+                "--source",
+                str(dst_file_path),
+                "--iou-thres",
+                0.5,
+                "--agnostic-nms",
+                "--max-det",
+                "20",
+                "--nosave",
+                # "--img",
+                # "640",
+                "--device",
+                1,
+                "--conf-thres",
+                0.2,
+                "--weights",
+                str(model_path),
+                "--name",
+                run_name,
+                # "--save-txt",
+                # "--save-crop",
+                # "--save-conf"
+            ]
+            result = subprocess.run(cmd)
+            tmp_results = tmp_folder / "results.txt"
+            if tmp_results.exists():
+                with open(str(tmp_results), "r") as tmp_file:
+                    detections = tmp_file.readlines()
+            else:
+                msg = "No results.txt file found"
+                logging.log(level=logging.CRITICAL, msg=msg)
+                return JsonResponse({"error": msg})
+
+            # tmp_folder.unlink()
+
             data = {
                 "detections": detections,
             }
-            logging.log(level=logging.INFO, msg=f"{file_name} {size}")
+            logging.log(level=logging.INFO, msg=f"{file_name} {type(result)}")
 
     return JsonResponse(data)
