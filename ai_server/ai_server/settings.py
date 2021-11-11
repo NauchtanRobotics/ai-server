347dd394eef574416362212abb07cf90683f75c0
David MacNaughton <davidmacnaugh@gmail.com>
2021-11-12 07:48:27 +1000

    ISS-0003 ALLOWED_HOSTS inc 'localhost'.

--- ai_server/ai_server/settings.py
+++ ai_server/ai_server/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = config.get("SECURITY", "SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["192.168.0.*"]
+ALLOWED_HOSTS = ["localhost", "192.168.0.11"]
 
 
 # Application definition
